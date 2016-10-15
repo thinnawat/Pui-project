32d1e8f48b3af39c07b4c213ee95ad96470645f4
puirtaf <thinnawatcom@gmail.com>
2016-10-15 06:53:43 -0700

    Design UI Main Complete

--- app/src/main/java/com/example/toughbook/puitraffic/MainActivity.java
+++ app/src/main/java/com/example/toughbook/puitraffic/MainActivity.java
@@ -1,13 +1,48 @@
 package com.example.toughbook.puitraffic;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    //Explicit การประกาศตัวแปร
+    private ListView listView;
+    private Button button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+        listView = (ListView) findViewById(R.id.livtraffic);
+        button = (Button) findViewById(R.id.button);
+
+        //Button Controller
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                //Sound Effecf
+                MediaPlayer mediaPlayer = MediaPlayer.create(getBaseContext(),
+                        R.raw.mosquito);
+                mediaPlayer.start();
+
+                //Show WebView
+                String url = "https://www.youtube.com/watch?v=IcQXbgN1TZg";
+                Intent intent = Intent(Intent.ACTION_VIEW);
+                Intent.setData{url.parset(url)};
+                startActivity(intent);
+
+            }  //onclick
+        });
+
+
+    } //Main Method กลุ่มของคำสั่ง
+} //Main Class นี่คือคลาสหลัก
